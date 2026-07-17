32440c2f43983da579d66693d41e4447482bd8e9
Huỳnh Tấn Thành <huynhtanthanhh123@gmail.com>
2026-07-17 09:06:21 +0700

    Add lantern animation page

---
+++ js/lantern.js
@@ -0,0 +1,15 @@
+const lantern = document.getElementById("lantern");
+
+
+lantern.addEventListener("mouseenter",()=>{
+
+    lantern.style.animationPlayState="paused";
+
+});
+
+
+lantern.addEventListener("mouseleave",()=>{
+
+    lantern.style.animationPlayState="running";
+
+});
